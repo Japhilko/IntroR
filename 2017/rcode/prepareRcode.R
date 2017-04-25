300dead514054a6d1f2c659d3ddd8510363e4866
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2017-04-25 21:40:31 +0200

    Datenanalyse

--- 2017/rcode/prepareRcode.R
+++ 2017/rcode/prepareRcode.R
@@ -3,9 +3,12 @@
 # Mon Apr 24 11:49:48 2017
 
 library(knitr)
+
 setwd("C:/Users/kolbjp/Documents/GitHub/IntroR/2017/slides")
 purl("WarumR.Rmd")
 purl("GrundlagenR.Rmd")
 purl("Datentypen.Rmd")
+
 purl("Hilfe.Rmd")
 
+purl("slides/Datenanalyse.Rmd")
